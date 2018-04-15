af1387f126c261b61d0c53f31ef10330c71f92ea
pzd <donggnij@yahoo.com>
2018-04-15 21:51:09 +0800

    add three codes

---
+++ src/easy/MaxConsecutiveOnes485.java
@@ -0,0 +1,11 @@
+package easy;
+
+public class MaxConsecutiveOnes485 {
+  public int findMaxConsecutive(int[] nums){
+	  int maxHere=0,max=0;
+	  for(int n:nums){
+		  max=Math.max(max,maxHere=n==0?0:maxHere+1);
+		  return max;
+	  }
+  }
+}
