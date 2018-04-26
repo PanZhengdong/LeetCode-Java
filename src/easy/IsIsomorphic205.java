b7edcedf0d4b177ccd78a9d989f0f60db9d614dc
Zhengdong Pan <donggnij@foxmail.com>
2018-04-26 20:22:17 +0800

    add two codes

---
+++ src/easy/IsIsomorphic205.java
@@ -0,0 +1,18 @@
+package easy;
+
+public class IsIsomorphic205 {
+	public boolean isIsomorphic(String s, String t) {
+		for (int i = 0; i < s.length(); i++)
+			if (value(i, s) != value(i, t))
+				return false;
+		return true;
+	}
+
+	private int value(int pos, String source) {
+		char c = source.charAt(pos);
+		int index = source.indexOf(c);
+		if (index < pos)
+			return index;
+		return pos;
+	}
+}
